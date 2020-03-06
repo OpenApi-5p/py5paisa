473e8478196740f81c63ad5a412fa2939514daff
Rahul Tiwari <jprrahultiwari@gmail.com>
2020-03-06 18:37:57 +0530

    Encryption logic fixed!

--- py5paisa/helpers/auth_helpers.py
+++ py5paisa/helpers/auth_helpers.py
@@ -1,56 +1,30 @@
-import cryptography
-from cryptography.hazmat.primitives import hashes
-from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
-from cryptography.hazmat.backends import default_backend
-
 from Crypto.Cipher import AES
-from .padding import pad
-import os
-
 import base64
-
+from pbkdf2 import PBKDF2
+from Crypto.Cipher import AES
 from conf import encryption_key
+import os
 
 
 class EncryptionClient:
 
     def __init__(self):
-        self.enc_key = None
         self.iv = bytes([83, 71, 26, 58, 54, 35, 22, 11,
                          83, 71, 26, 58, 54, 35, 22, 11])
-        self.derived_key = None
-        self.cipher = None
-        self.backend = default_backend()
+        self.enc_key = encryption_key
 
     def _pad_and_convert_to_bytes(self, text):
-        plaintext = bytes(text, encoding="utf-8")
-        padded = pad(plaintext, AES.block_size, style='pkcs7')
-        return padded
-
-    def _get_enc_key(self):
-        enc_key = encryption_key
-        if not enc_key:
-            raise Exception("ENCRYPTION_KEY not set in environment variables")
-        self.enc_key = base64.b64encode(
-            bytes(enc_key, encoding="utf-8"))
-
-    def _get_derived_key(self):
-        self.derived_key = PBKDF2HMAC(
-            algorithm=hashes.SHA1,
-            length=32,
-            salt=self.iv,
-            iterations=1000,
-            backend=self.backend).derive(self.enc_key)
-
-    def get_cipher(self):
-        self.cipher = AES.new(self.derived_key, AES.MODE_CBC, self.iv)
+        return bytes(text+chr(16-len(text) % 16)*(16-len(text) % 16), encoding="utf-8")
 
     def encrypt(self, text):
-        self._get_enc_key()
-        self._get_derived_key()
-        self.get_cipher()
         padded_text = self._pad_and_convert_to_bytes(text)
-        return base64.b64encode(self.cipher.encrypt(padded_text))
+        key_gen = PBKDF2(self.enc_key, self.iv)
+
+        aesiv = key_gen.read(16)
+        aeskey = key_gen.read(32)
+        cipher = AES.new(aeskey, AES.MODE_CBC, iv=aesiv)
+
+        return base64.b64encode(cipher.encrypt(padded_text))
 
 
 def get_cookie():
