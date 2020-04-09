c86d263b3e28ef7032878231416db44c2abb3a10
invinciblycool <jprrahultiwari@gmail.com>
2020-04-10 02:55:23 +0530

    Update README and travis config

--- setup.py
+++ setup.py
@@ -19,13 +19,11 @@ test_requirements = [ ]
 setup(
     author="Rahul Tiwari",
     author_email='jprrahultiwari@gmail.com',
-    python_requires='>=3.5',
+    python_requires='>=3.6',
     classifiers=[
         'Development Status :: 2 - Pre-Alpha',
         'Intended Audience :: Developers',
         'Natural Language :: English',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
