bc766e58100d5dd274304fff2101c33ce4d940d5
invinciblycool <jprrahultiwari@gmail.com>
2020-04-30 11:06:54 +0530

    Release 0.1.2

--- setup.py
+++ setup.py
@@ -38,6 +38,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/invinciblycool/py5paisa',
-    version='0.1.1',
+    version='0.1.2',
     zip_safe=False,
 )
