d9ad10a63c1c7aa9f4b18ab9d2cd7986e6514697
invinciblycool <jprrahultiwari@gmail.com>
2020-07-09 14:58:01 +0530

    Bump up to 0.1.1

--- setup.py
+++ setup.py
@@ -46,6 +46,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/5paisa/py5paisa',
-    version='0.1.0',
+    version='0.1.1',
     zip_safe=False,
 )
