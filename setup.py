dff0717d230f3b7db6c78060d0897b1ed66e1fb7
invinciblycool <jprrahultiwari@gmail.com>
2020-09-04 13:35:17 +0530

    Correct version to 0.1.6

--- setup.py
+++ setup.py
@@ -47,6 +47,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/5paisa/py5paisa',
-    version='0.0.6',
+    version='0.1.6',
     zip_safe=False,
 )
