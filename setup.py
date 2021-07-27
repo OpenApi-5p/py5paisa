27823f0f322b40390965555d783a4733b7059fab
5paisa <65898074+5paisa@users.noreply.github.com>
2021-07-27 10:55:25 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -49,6 +49,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/5paisa/py5paisa',
-    version='0.3.0',
+    version='0.2.10',
     zip_safe=False,
 )
