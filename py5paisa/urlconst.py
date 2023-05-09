b6e766c07f65b9b07c8b4e51d9ddb4802ff7f992
Gaurav <gaurav.dwivedi2@5paisa.com>
2023-05-09 11:58:58 +0530

    Added wrapper methods for totp and request token

--- py5paisa/urlconst.py
+++ py5paisa/urlconst.py
@@ -1,6 +1,5 @@
 
-BaseUrl='https://Openapi.5paisa.com/VendorsAPI/Service1.svc/'
-
+BaseUrl = 'https://Openapi.5paisa.com/VendorsAPI/Service1.svc/'
 
 
 LOGIN_ROUTE = f'{BaseUrl}V4/LoginRequestMobileNewbyEmail'
@@ -11,46 +10,46 @@ HOLDINGS_ROUTE = f'{BaseUrl}V3/Holding'
 POSITIONS_ROUTE = f'{BaseUrl}V1/NetPositionNetWise'
 
 ORDER_PLACEMENT_ROUTE = f'{BaseUrl}V1/PlaceOrderRequest'
-ORDER_MODIFY_ROUTE= f'{BaseUrl}V1/ModifyOrderRequest'
-ORDER_CANCEL_ROUTE= f'{BaseUrl}V1/CancelOrderRequest'
+ORDER_MODIFY_ROUTE = f'{BaseUrl}V1/ModifyOrderRequest'
+ORDER_CANCEL_ROUTE = f'{BaseUrl}V1/CancelOrderRequest'
 ORDER_STATUS_ROUTE = f'{BaseUrl}V1/OrderStatus'
 TRADE_INFO_ROUTE = f'{BaseUrl}TradeInformation'
 
-BRACKET_MOD_ROUTE= f'{BaseUrl}ModifyBracketOrderRequest'
-BRACKET_ORDER_ROUTE= f'{BaseUrl}BracketOrderRequest'
-BRACKET_CANCEL_ROUTE= f'{BaseUrl}CancelBracketOrderRequest'
+BRACKET_MOD_ROUTE = f'{BaseUrl}ModifyBracketOrderRequest'
+BRACKET_ORDER_ROUTE = f'{BaseUrl}BracketOrderRequest'
+BRACKET_CANCEL_ROUTE = f'{BaseUrl}CancelBracketOrderRequest'
 
-COVER_MOD_ROUTE= f'{BaseUrl}ModifyCoverOrderequest'
-COVER_ORDER_ROUTE= f'{BaseUrl}CoverOrderRequest'
-COVER_CANCEL_ROUTE= f'{BaseUrl}CancelCoverOrderRequest'
+COVER_MOD_ROUTE = f'{BaseUrl}ModifyCoverOrderequest'
+COVER_ORDER_ROUTE = f'{BaseUrl}CoverOrderRequest'
+COVER_CANCEL_ROUTE = f'{BaseUrl}CancelCoverOrderRequest'
 
-MARKET_FEED_ROUTE= f'{BaseUrl}MarketFeed'
-LOGIN_CHECK_ROUTE="https://openfeed.5paisa.com/Feeds/api/UserActivity/LoginCheck"
+MARKET_FEED_ROUTE = f'{BaseUrl}MarketFeed'
+LOGIN_CHECK_ROUTE = "https://openfeed.5paisa.com/Feeds/api/UserActivity/LoginCheck"
 
-MARKET_DEPTH_ROUTE= f'{BaseUrl}/MarketDepth'
-MARKET_DEPTH_BY_SYMBOL_ROUTE= f'{BaseUrl}V1/MarketDepth'
-JWT_VALIDATION_ROUTE="https://Openapi.indiainfoline.com/VendorsAPI/Service1.svc/JWTOpenApiValidation"
-HISTORICAL_DATA_ROUTE="https://openapi.5paisa.com/historical/"
+MARKET_DEPTH_ROUTE = f'{BaseUrl}/MarketDepth'
+MARKET_DEPTH_BY_SYMBOL_ROUTE = f'{BaseUrl}V1/MarketDepth'
+JWT_VALIDATION_ROUTE = "https://Openapi.indiainfoline.com/VendorsAPI/Service1.svc/JWTOpenApiValidation"
+HISTORICAL_DATA_ROUTE = "https://openapi.5paisa.com/historical/"
 
-IDEAS_ROUTE= f'{BaseUrl}TraderIDEAs'
+IDEAS_ROUTE = f'{BaseUrl}TraderIDEAs'
 
-TRADEBOOK_ROUTE= f'{BaseUrl}V1/TradeBook'
+TRADEBOOK_ROUTE = f'{BaseUrl}V1/TradeBook'
+GET_REQUEST_TOKEN_ROUTE = f'{BaseUrl}TOTPLogin'
+ACCESS_TOKEN_ROUTE = f'{BaseUrl}GetAccessToken'
+MARKET_STATUS_ROUTE = f'{BaseUrl}MarketStatus'
+TRADE_HISTORY_ROUTE = f'{BaseUrl}TradeHistory'
 
-ACCESS_TOKEN_ROUTE=  f'{BaseUrl}GetAccessToken'
-MARKET_STATUS_ROUTE=  f'{BaseUrl}MarketStatus'
-TRADE_HISTORY_ROUTE=  f'{BaseUrl}TradeHistory'
-
-GET_BASKET_ROUTE=  f'{BaseUrl}GetBaskets'
-CREATE_BASKET_ROUTE= f'{BaseUrl}CreateBasket'
-RENAME_BASKET_ROUTE= f'{BaseUrl}EditBasketName'
-DELETE_BASKET_ROUTE= f'{BaseUrl}DeleteBasket'
-CLONE_BASKET_ROUTE= f'{BaseUrl}CloneBasket'
-EXECUTE_BASKET_ROUTE= f'{BaseUrl}ExecuteBasket'
-GET_ORDER_IN_BASKET_ROUTE= f'{BaseUrl}GetOrderInBasket'
-ADD_BASKET_ORDER_ROUTE= f'{BaseUrl}AddOrderToBasket'
-OPTION_CHAIN_ROUTE=f'{BaseUrl}V2/GetExpiryForSymbolOptions'
-GET_OPTION_CHAIN_ROUTE=f'{BaseUrl}GetOptionsForSymbol'
-CANCEL_BULK_ORDER_ROUTE=f'{BaseUrl}CancelOrderBulk'
-SQUAREOFF_ROUTE=f'{BaseUrl}SquareOffAll'
-POSITION_CONVERSION_ROUTE=f'{BaseUrl}PositionConversion'
-MARKET_DEPTH_ROUTE_20="https://openapi.5paisa.com/marketfeed-token/token"
+GET_BASKET_ROUTE = f'{BaseUrl}GetBaskets'
+CREATE_BASKET_ROUTE = f'{BaseUrl}CreateBasket'
+RENAME_BASKET_ROUTE = f'{BaseUrl}EditBasketName'
+DELETE_BASKET_ROUTE = f'{BaseUrl}DeleteBasket'
+CLONE_BASKET_ROUTE = f'{BaseUrl}CloneBasket'
+EXECUTE_BASKET_ROUTE = f'{BaseUrl}ExecuteBasket'
+GET_ORDER_IN_BASKET_ROUTE = f'{BaseUrl}GetOrderInBasket'
+ADD_BASKET_ORDER_ROUTE = f'{BaseUrl}AddOrderToBasket'
+OPTION_CHAIN_ROUTE = f'{BaseUrl}V2/GetExpiryForSymbolOptions'
+GET_OPTION_CHAIN_ROUTE = f'{BaseUrl}GetOptionsForSymbol'
+CANCEL_BULK_ORDER_ROUTE = f'{BaseUrl}CancelOrderBulk'
+SQUAREOFF_ROUTE = f'{BaseUrl}SquareOffAll'
+POSITION_CONVERSION_ROUTE = f'{BaseUrl}PositionConversion'
+MARKET_DEPTH_ROUTE_20 = "https://openapi.5paisa.com/marketfeed-token/token"
