02e0a8958dfcf8b5f6d76f2fc68761974132e10a
5paisa <65898074+5paisa@users.noreply.github.com>
2024-01-19 15:03:03 +0530

    Update setup.py
    update for scripmaster api

--- setup.py
+++ setup.py
@@ -50,6 +50,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/5paisa/py5paisa',
-    version='0.7.8',
+    version='0.7.9',
     zip_safe=False,
 )
