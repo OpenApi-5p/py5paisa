67fc68cb41519f9a298464cb109090e0800c1055
jENISH SHAH <jenish.shah@5paisa.com>
2025-03-25 18:23:35 +0530

    httpx & multi order

--- setup.py
+++ setup.py
@@ -51,6 +51,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/5paisa/py5paisa',
-    version='0.7.19',
+    version='0.7.20',
     zip_safe=False,
 )
