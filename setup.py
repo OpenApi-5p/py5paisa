ecfa290071b1def0909cdf7ad04dc9bc5d791e69
5paisa <65898074+5paisa@users.noreply.github.com>
2025-08-11 13:37:21 +0530

    Update setup.py
    updating version to .21

--- setup.py
+++ setup.py
@@ -51,6 +51,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/5paisa/py5paisa',
-    version='0.7.22',
+    version='0.7.21',
     zip_safe=False,
 )
